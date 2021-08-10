d35e8fa916e2b4e82069387d4ed108f93d318ded
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-11 00:04:26 +0900

    [update] 2021/08/11

--- playground.ts
+++ playground.ts
@@ -1,6 +1,16 @@
+type Person = {
+  name: string
+  age: number
+}
+
 export default function play() {
   const name: string = 'hello'
   const age: number = 30
+
+  const person: Person = {
+    name: 'John',
+    age: 34
+  }
 
   function logPersonInfo(personName: string, personAge: number) {
     const info = `Name: ${personName}, age: ${personAge}`
@@ -8,5 +18,12 @@ export default function play() {
     return info
   }
 
+  function logPersonInfo2(person: Person) {
+    const info = `Name: ${person.name}, age: ${person.age}`
+    console.info(info)
+    return info
+  }
+
   logPersonInfo(name, age)
+  logPersonInfo2(person)
 }
