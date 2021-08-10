5f1b1fab40bcd6f74e2b669e70b29f362d83f401
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-10 23:41:42 +0900

    [update] 2021/08/10

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   const message: string = 'Hello world'
 
+  let a: AddEventListenerOptions
+  let b: NodeJS.Process
+
+  console.info(b.browser)
+
   return <div>{message}</div>
 }
