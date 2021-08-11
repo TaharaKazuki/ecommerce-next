83a57f4646ef8eedb493633dd9d4944429a5eca3
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-11 21:45:51 +0900

    [update] 2021/08/11

--- playground.ts
+++ playground.ts
@@ -13,18 +13,37 @@ interface AcademicPerson extends Person {
 
 type RaceCar = {
   name: string
-  maxSpeed: number
+  team: string
+  maxSpeed: 200
 }
 
 type CityCar = {
   name: string
   space: string
+  maxSpeed: 100
 }
 
 type Car = RaceCar | CityCar
 
 export default function play() {
+  const car: RaceCar = {
+    name: 'Race car',
+    maxSpeed: 200,
+    team: 'ferari'
+  }
+
   function logCarInfo(car: Car) {
     console.info(car.name)
+
+    switch (car.maxSpeed) {
+      case 200:
+        console.info(car)
+        break
+      case 100:
+        console.info(car)
+        break
+      default:
+        console.info(car)
+    }
   }
 }
