afafa38d7ad9a78bf010dc1124e8c77eb740b458
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-11 22:02:04 +0900

    [update]

--- playground.ts
+++ playground.ts
@@ -1,15 +1,23 @@
 interface Person {
+  kind: 'business' | 'academic' | 'otherType'
   name: string
   age: number
 }
 
 interface BusinessPerson extends Person {
+  kind: 'business'
   salary: number
 }
 
 interface AcademicPerson extends Person {
+  kind: 'academic'
   publications: string[]
 }
+
+type Human =
+  | BusinessPerson
+  | AcademicPerson
+  | { kind: 'otherType'; special: string }
 
 type RaceCar = {
   name: string
@@ -32,10 +40,19 @@ export default function play() {
     team: 'ferari'
   }
 
-  function logCarInfo(car: Car) {
-    console.info((car as RaceCar).team)
-    console.info((<CityCar>car).space)
+  function logPersonInfo(human: Human) {
+    if (human.kind === 'academic') {
+      console.info(human)
+    } else if (human.kind === 'business') {
+      console.info(human)
+    } else if (human.kind === 'otherType') {
+      console.info(human)
+    } else {
+      console.info(human)
+    }
+  }
 
+  function logCarInfo(car: Car) {
     switch (car.maxSpeed) {
       case 200:
         console.info(car)
