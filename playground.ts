752f874f555520ac9b90bb3da10f9c1338e58ada
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2021-08-12 10:12:25 +0900

    [update] Void type

--- playground.ts
+++ playground.ts
@@ -19,87 +19,17 @@ interface Person {
   prop2: number
 }
 
-
-type Human =
-  | BusinessPerson
-  | AcademicPerson
-  | { kind: 'otherType'; special: string }
-
-type RaceCar = {
-  name: string
-  team: string
-  maxSpeed: 200
-}
-
-type CityCar = {
-  name: string
-  space: string
-  maxSpeed: 100
-}
-
-type SUVCar = {
-  name: string
-  maxSpeed: 150
-  isCarbonFree: boolean
-}
-
-
-type Car = RaceCar | CityCar | SUVCar
+type Noop = () => any
+type Noop2 = () => void
 
 export default function play() {
-  const car: RaceCar = {
-    name: 'Race car',
-    maxSpeed: 200,
-    team: 'ferari'
+  function fn1(x:Noop): void {
+    const result = x()
+    result()
   }
 
-  const person: Person = {
-    prop1: '',
-    prop2: 2,
-    name: '',
-    kind: 'academic',
-    age: 23
-  }
-
-  function printInfo(someObject: {[key: string]: unknown}) {
-    if (typeof someObject.age === 'string') {
-      someObject.age.toUpperCase()
-    }
-  }
-
-  printInfo({
-    age: 23,
-    isMarried: true,
-    name: 'Filip'
-  })
-
-
-  function logPersonInfo(human: Human) {
-    if (human.kind === 'academic') {
-      console.info(human)
-    } else if (human.kind === 'business') {
-      console.info(human)
-    } else if (human.kind === 'otherType') {
-      console.info(human)
-    } else {
-      console.info(human)
-    }
-  }
-
-  function logCarInfo(car: Car) {
-    switch (car.maxSpeed) {
-      case 200:
-        console.info(car)
-        break
-      case 100:
-        console.info(car)
-        break
-      case 150:
-        console.info(car)
-        break
-      default:
-        const _never: never = car
-        return
-    }
+  function fn2(x:Noop2):void {
+    // const result = x()
+    // result()
   }
 }
