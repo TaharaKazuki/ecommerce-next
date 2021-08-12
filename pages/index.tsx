57b74da244c3117ab2077f1a1f3382123511d1bc
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-13 00:05:52 +0900

    [update] Infer getstaticprops

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import { useEffect } from 'react'
-import play from '../playground'
+import type { InferGetStaticPropsType } from 'next'
 
-export default function Home() {
-  useEffect(() => {
-    play()
-  }, [])
+export async function getStaticProps() {
+  const products = [1, 2, 3]
 
-  return <div>Hello World</div>
+  return {
+    props: {
+      products
+    },
+    revalidate: 4 * 60 * 60
+  }
+}
+
+export default function Home({
+  products
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <div>{products}</div>
 }
