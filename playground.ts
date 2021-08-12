4e19530b82125515d6d28f5702a8cf697a41e141
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2021-08-12 12:24:05 +0900

    [update] Custom generic type

--- playground.ts
+++ playground.ts
@@ -1,30 +1,36 @@
 interface Person {
-  kind: 'business' | 'academic' | 'otherType'
   name: string
   age: number
 }
 
-interface BusinessPerson extends Person {
-  kind: 'business'
-  salary: number
-}
-
-interface AcademicPerson extends Person {
-  kind: 'academic'
-  publications: string[]
-}
-
-interface Person {
-  prop1: string
-  prop2: number
+class Logger<T> {
+  log(items: Array<T>, callback: (i:T) => void) {
+    items.forEach((itme) => {
+      callback(itme)
+    })
+  }
 }
 
 export default function play() {
-  function iterate(items: Array<string>) {
-    items.forEach((item) => {
-      console.info(item.toUpperCase())
-    })
-  }
+  const logger = new Logger<string>()
 
-  iterate(['filip', 'john', 'tom'])
+  const cars = ['audi', 'skoda', 'citroen']
+  logger.log(cars, (car) => {
+    console.info(car)
+  })
+
+  const logger2 = new Logger<number>()
+
+  const numbers = [1,2,3,4]
+  logger2.log(numbers, (num) => {
+    console.info(num)
+  })
+
+  const logger3 = new Logger<Person>()
+
+  const persons = [{name: 'filip', age: 30}, {name: 'john', age: 20}]
+
+  logger3.log(persons, (person) => {
+    console.info(person)
+  }) 
 }
